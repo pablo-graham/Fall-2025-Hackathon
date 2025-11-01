455b59c292b62bfc9f657bbda7eaf609407e4600
pablo-graham <phrs327@gmail.com>
2025-11-01 13:44:46 -0700

    pages

--- food_harmony_main/vite.config.ts
+++ food_harmony_main/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Fall-2025-Hackathon",
   server: {
     host: "::",
     port: 8080,
